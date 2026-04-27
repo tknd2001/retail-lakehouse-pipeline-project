987ca3c8ac9dafaea146f8f3902e6b69e98b5552
tknd2001 <142404227+tknd2001@users.noreply.github.com>
2026-04-27 11:35:53 -0400

    Update 01_bronze_ingestion notebook.py

--- notebook/01_bronze_ingestion notebook.py
+++ notebook/01_bronze_ingestion notebook.py
@@ -1,9 +1,7 @@
 from pyspark.sql.functions import current_timestamp, lit
 
-
 raw_base_path = "/Volumes/workspace/default/raw_data"
 bronze_base_path = "/Volumes/workspace/default/raw_data/bronze"
-
 
 files = {
     "customers": "customers.csv",
@@ -12,21 +10,19 @@ files = {
     "sales_transactions": "sales_transactions.csv"
 }
 
-
 for table_name, file_name in files.items():
     df = spark.read.format("csv") \
         .option("header", "true") \
         .option("inferSchema", "true") \
         .load(f"{raw_base_path}/{file_name}")
 
-
     bronze_df = df.withColumn("ingestion_timestamp", current_timestamp()) \
-                  .withColumn("source_file", lit(file_name))
-
+                  .withColumn("source_file", lit(file_name)) \
+                  .withColumn("pipeline_stage", lit("bronze"))
 
     bronze_df.write.format("delta") \
         .mode("overwrite") \
+        .option("overwriteSchema", "true") \
         .save(f"{bronze_base_path}/{table_name}")
-
 
     print(f"Bronze table recreated: {table_name}")
